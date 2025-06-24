d8539b02411a47759c503175c1b3b51f37ecded2
Arcaino1013 <aeneasreina@gmail.com>
2025-06-24 11:00:26 +0200

    TestScript

---
+++ Test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+
+enroot import --output /home/reinaae80667/images/python_image.sqsh docker://python:3.11-slim
+
+sbatch <<EOF
+#!/bin/bash
+#SBATCH --job-name=python_container_job
+#SBATCH --output=container_output.log
+#SBATCH --ntasks=1
+#SBATCH --time=00:05:00
+#SBATCH --partition=standard
+#SBATCH --container-image=/home/reinaae80667/images/python_image.sqsh
+#SBATCH --container-name=python-container
+
+python -c 'print("hallo container")'
+EOF
